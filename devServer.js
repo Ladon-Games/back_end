fb7484e21cc61b938f07988aea197d94502d6ec6
ToMattBan <cleyton_laz@protonmail.com>
2021-12-30 19:50:47 -0300

    Changed script to match githubpages

--- devServer.js
+++ devServer.js
@@ -3,7 +3,7 @@ const app = express();
 
 const routes = require("./routes/router");
 
-app.use('/', routes);
+app.use('/back_end', routes);
 
 app.listen(3000, function () {
     console.log("Server started in http://localhost:3000/");
