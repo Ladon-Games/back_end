2073480fe57ec00dd9bc5e54cc6af0f7db5ae78c
ToMattBan <cleyton_laz@protonmail.com>
2021-12-30 20:27:31 -0300

    Added heroku as dependency to try to deploy

--- devServer.js
+++ devServer.js
@@ -5,6 +5,6 @@ const routes = require("./routes/router");
 
 app.use('/', routes);
 
-app.listen(3000, function () {
+app.listen(5000, function () {
     console.log("Server started in http://localhost:3000/");
 })
