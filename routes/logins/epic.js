fb61d9cc5c0506639bae014275ab6b50dcd67986
ToMattBan <cleyton_laz@protonmail.com>
2022-01-25 21:04:11 -0300

    Login sendo feito com SID, porém falta de permissão para pegar a biblioteca

--- routes/logins/epic.js
+++ routes/logins/epic.js
@@ -1,35 +1,76 @@
 var epicLogin = require('express').Router()
   , https = require('https');
 
-const requestOptions = {
-  headers: {
-    'X-Epic-Event-Action': 'login',
-    'X-Epic-Event-Category': 'login',
-    'X-Epic-Strategy-Flags': '',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Host': 'www.epicgames.com'
+async function getFirstsCookies(sid) {
+  const requestOptions = {
+    hostname: 'www.epicgames.com',
+    path: `/id/api/set-sid?sid=${sid}`,
+    headers: {
+      'X-Epic-Event-Action': 'login',
+      'X-Epic-Event-Category': 'login',
+      'X-Epic-Strategy-Flags': '',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Host': 'www.epicgames.com'
+    }
   }
-}
 
-epicLogin.get('/', function (req, res) {
-  requestOptions.hostname = 'www.epicgames.com';
-  requestOptions.path = `/id/api/set-sid?sid=${req.query.sid}`
-
-  https.get(requestOptions, (response) => {
-    var cookiesOriginal = response.headers['set-cookie'];
-    var cookies = {};
+  await https.get(requestOptions, async (response) => {
+    var cookiesOriginal = response.headers['set-cookie']
+      , cookies = {};
 
     cookiesOriginal.forEach(cookie => {
       var name = cookie.match(/.*?(?==)/g)[0];
-      cookies[name] = cookie;
+      cookies[name] = cookie.match(/[^=$;]+/g)[1];
     })
 
-    if (!cookies["EPIC_BEARER_TOKEN"]) 
-      return res.send('Failed autentication')
+    if (!cookies["EPIC_BEARER_TOKEN"])
+      return 'Failed autentication'
 
-    global.userAccounts.epic = cookies;
-    res.redirect('/')
+    return await getAnotherCookies(cookies);
   })
+};
+
+async function getAnotherCookies(cookies) {
+  await https.get('https://www.epicgames.com/id/api/csrf', async (response) => {
+    var cookiesOriginal = response.headers['set-cookie'];
+    
+    cookiesOriginal.forEach(cookie => {
+      var name = cookie.match(/.*?(?==)/g)[0];
+      cookies[name] = cookie.match(/[^=$;]+/g)[1];
+    })
+
+    if (!cookies['XSRF-TOKEN']) 
+      return 'Failed autentication'
+
+    return await getGames(cookies);
+  })
+}
+
+async function getGames(cookies) {
+  const requestOptions = {
+    host: 'library-service.live.use1a.on.epicgames.com',
+    hostname: 'library-service.live.use1a.on.epicgames.com',
+    path: '/library/api/public/items',
+    headers: {
+      Host: 'library-service.live.use1a.on.epicgames.com',
+      Authorization: `bearer ${cookies.EPIC_BEARER_TOKEN}`
+    }
+  }
+
+  console.log(requestOptions)
+
+  await https.get(requestOptions, async (response) => {
+    response.setEncoding('utf-8')
+    response.on('data', (data) => {
+      console.log(data);
+    })
+  })
+}
+
+epicLogin.get('/', async function (req, res) {
+  const response = await getFirstsCookies(req.query.sid);
+  //global.userAccounts.epic = cookies;
+  //res.redirect('/')
 })
 
 module.exports = epicLogin;
